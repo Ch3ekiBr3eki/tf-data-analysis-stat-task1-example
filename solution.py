f172a16b312d659b0924b7a19dd4d7e566ee03dc
Anton <113473409+Ch3ekiBr3eki@users.noreply.github.com>
2023-04-06 15:09:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 323297403 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x += 567
-    log_x = np.log(x)
-    log_mean = np.mean(log_x)
-    a = np.exp(log_mean)
+    log_x = np.log(x - 567)
+
+    a = log_x.mean()
+
     return a
